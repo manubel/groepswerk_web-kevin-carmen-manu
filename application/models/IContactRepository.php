fdc3c958d8b90bbb36582309c54c70d6bbf354e6
infectedinfested <kevin.vandenheede@gmail.com>
2017-11-16 15:02:45 +0100

    routes API

--- application/models/IContactRepository.php
+++ application/models/IContactRepository.php
@@ -12,7 +12,7 @@ interface IContactRepository
     public function get_contact_by_id($id);
 
     public function create_contact($data);
-    public function update_contact($data);
+    public function update_contact($id,$data);
     public function delete_contact($id);
 
 
